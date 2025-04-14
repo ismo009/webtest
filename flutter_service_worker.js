f64e19c1755e6d9f9bea0ed407557e337e8bade2
ismo009 <75813472+ismo009@users.noreply.github.com>
2025-04-14 19:53:12 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -43,7 +43,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d6e72428090389e7075ab7d691fb4ee6
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "0435e30bc86c18712e9817084d9b457d",
+"flutter_bootstrap.js": "1a3c3c1415d94d20aeb2d244a894c603",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
